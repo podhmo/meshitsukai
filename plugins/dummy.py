bf4655dec3a66d8079adf86216eda630c02c95dd
podhmo <podhmo+altair@beproud.jp>
2015-06-13 18:43:40 +0900

    plugin option is support(section name is <PluginClass>.__name__.lower()

--- plugins/dummy.py
+++ plugins/dummy.py
@@ -1,13 +1,13 @@
 # -*- coding:utf-8 -*-
 from meshitsukai.plugin import Plugin
 from meshitsukai.view import as_view
+import logging
+logger = logging.getLogger(__name__)
 
 
-class DummyPlugin(Plugin):
+class Dummy(Plugin):
     @as_view()
     def process_message(self, request):
-        print("---------- incomming: ----------")
         # data : {'ts': '1433670154.000003', 'type': 'message', 'team': 'XxxXXXXXX', 'text': 'aa', 'user': 'XxxXXxxXX', 'channel': 'XxxXXXXXx'}
-        print(request._rawdata)
-        print("--------------------------------")
+        logger.debug("incomming: %s", request._rawdata)
         return "`*dummy*`"
--- plugins/hello.py
+++ plugins/hello.py
@@ -3,10 +3,14 @@ from meshitsukai.plugin import Plugin
 from meshitsukai.view import as_view
 
 
-class HelloPlugin(Plugin):
+class Hello(Plugin):
     def in_hello(self, request):
         return "hello" in request.body.lower()
 
+    @property
+    def template(self):
+        return self.settings.get("template") or "hello <@{user}>"
+
     @as_view(predicate="in_hello")
     def process_message(self, request):
-        return "<@{user}> `*hello*`".format(user=request.user)
+        return self.template.format(user=request.user)
